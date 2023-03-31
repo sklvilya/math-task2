48ddaac7043e943307752f137f407c188d8305c2
sklvilya <sklvilyaomsk@gmail.com>
2023-03-31 19:56:17 +0600

    fixed files demon & magician .js

--- src/demon.js
+++ src/demon.js
@@ -1,10 +1,7 @@
 import ChangeDamage from './ChangeDamage';
 
 export default class Demon extends ChangeDamage {
-  // constructor(distance) {
-  //   super();
-  //   this.distance = distance;
-  //   this.stoned = distance;
-  //   this.attack = distance;
-  // }
+  constructor(name, type, distance) {
+    super(name, 'Demon', distance);
+  }
 }
--- src/magician.js
+++ src/magician.js
@@ -1,10 +1,7 @@
 import ChangeDamage from './ChangeDamage';
 
 export default class Magician extends ChangeDamage {
-//   constructor(distance) {
-//     super();
-//     this.distance = distance;
-//     this.stoned = distance;
-//     this.attack = distance;
-//   }
+  constructor(name, type, distance) {
+    super(name, 'Magician', distance);
+  }
 }
